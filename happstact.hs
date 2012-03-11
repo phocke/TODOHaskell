c040c1b8e6e89a93576299a9258ebe615429dd9e
Przemek Hocke <phocke@gmail.com>
2012-03-11 00:28:11 +0000

    basic error handling and routes

--- happstact.hs
+++ happstact.hs
@@ -1,6 +1,10 @@
 module Main where
 
-import Happstack.Server (nullConf, simpleHTTP, toResponse, ok)
+import Control.Monad
+import Happstack.Server (nullConf, simpleHTTP, ok, dir, notFound )
 
 main :: IO ()
-main = simpleHTTP nullConf $ ok "Hello suckers"
+main = simpleHTTP nullConf $ msum [ dir "hello" $ ok "hello world" 
+                                  , dir "bye" $ ok "bye bye world"
+                                  , notFound "you crazy or wha' ?" 
+                                  ]
